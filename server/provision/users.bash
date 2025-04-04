734a01f7275684fa0733ba51540d6d0d7ca3b071
Software Shinobi <the.software.shinobi@gmail.com>
2025-04-04 10:49:00 -0400

    automated terminal push

--- server/provision/users.bash
+++ server/provision/users.bash
@@ -2,8 +2,8 @@
 
 echo "proving grounds -> monaco heist -> installed."
 
-sudo useradd -m -p 'monaco0' monaco0
-sudo useradd -m -p 'O9P7a1RxFM8O' monaco1
+##sudo useradd -m -p 'monaco0' monaco0
+sudo useradd -m -p 'monaco1' monaco1
 sudo useradd -m -p '4tkcctZAl1p4' monaco2
 sudo useradd -m -p 'iGO4ZZIkPn9W' monaco3
 sudo useradd -m -p 'leUjjGFPAXdd' monaco4
